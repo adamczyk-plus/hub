58ce35d7bb48f982e44dcb864d2c0326b970ccec
Lukasz <poczta@adamczyk.plus>
2025-12-23 19:50:28 +0100

    disabled db connection for a moment to test

--- app/auth/actions/login.ts
+++ app/auth/actions/login.ts
@@ -14,14 +14,14 @@ export async function login(
   _1: { error?: string },
   { username, password }: { username: string; password: string }
 ) {
-  const db = await createConnection();
-  const sql = "select password_hash hash from users where username = ?";
-  const [rows] = await db.query<UserRow[]>(sql, [username]);
+  // const db = await createConnection();
+  // const sql = "select password_hash hash from users where username = ?";
+  // const [rows] = await db.query<UserRow[]>(sql, [username]);
 
-  if (!rows.length) return { error: "Nie ma takiego użytkownika" };
+  // if (!rows.length) return { error: "Nie ma takiego użytkownika" };
 
-  const match = await bcrypt.compare(password, rows[0].hash);
-  if (!match) return { error: "Niepoprawne hasło" };
+  // const match = await bcrypt.compare(password, rows[0].hash);
+  // if (!match) return { error: "Niepoprawne hasło" };
 
   const payload: JwtPayload = { userId: 1, username: "ladamczyk" };
   const token = await encrypt(payload);
