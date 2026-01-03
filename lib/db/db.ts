52e4b06b9ea3b81e5640904c8c029e7b8ed97a25
Lukasz <poczta@adamczyk.plus>
2026-01-03 11:04:52 +0100

    env exports

--- lib/db/db.ts
+++ lib/db/db.ts
@@ -1,10 +1,12 @@
 import { Pool, QueryResultRow } from "pg";
+import { isProduction } from "../env";
 
 const pool = new Pool({
   user: process.env.PG_USER,
   host: process.env.PG_HOST,
   database: process.env.PG_DATABASE,
   password: process.env.PG_PASSWORD,
+  ssl: isProduction && { rejectUnauthorized: false },
 });
 
 export async function query<T extends QueryResultRow = QueryResultRow>(
---
+++ lib/env.ts
@@ -0,0 +1,2 @@
+export const isProduction = process.env.NEXT_PUBLIC_APP_ENV === "production";
+export const isLocal = process.env.NEXT_PUBLIC_APP_ENV === "local";
