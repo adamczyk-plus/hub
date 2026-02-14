625f8604c943586f65eb733943e131a05c6240ae
Lukasz <poczta@adamczyk.plus>
2026-02-14 16:50:37 +0100

    jwt exp increased from 1 to 4h

--- lib/auth/jwt.ts
+++ lib/auth/jwt.ts
@@ -13,7 +13,7 @@ export async function encrypt(payload: JwtPayload) {
   return await new SignJWT(payload)
     .setProtectedHeader({ alg: "HS256" })
     .setIssuedAt()
-    .setExpirationTime("1h")
+    .setExpirationTime("4h")
     .sign(secret);
 }
 
